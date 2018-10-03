948dfe4cf58b43c2afd24d8c17e1347e86d73100
davkim2 <davkim2@WKWUS4191978.global.publicisgroupe.net>
2018-10-03 15:21:47 +0530

    init

---
+++ TradingService/src/main/java/com/sapient/TradingService/TradingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sapient.TradingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingServiceApplication.class, args);
+	}
+}
