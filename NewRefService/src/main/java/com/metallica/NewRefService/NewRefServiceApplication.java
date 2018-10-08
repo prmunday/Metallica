9ca0fef785fd4dfb869630a3bf010f2a44ca21d2
pauwarne <pauwarne@WKWUS8123840.global.publicisgroupe.net>
2018-10-08 16:04:17 +0530

    Integration of RefData service with Zuul and Eureka.

--- NewRefService/src/main/java/com/metallica/NewRefService/NewRefServiceApplication.java
+++ NewRefService/src/main/java/com/metallica/NewRefService/NewRefServiceApplication.java
@@ -2,8 +2,12 @@ package com.metallica.NewRefService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class NewRefServiceApplication {
 
 	public static void main(String[] args) {
