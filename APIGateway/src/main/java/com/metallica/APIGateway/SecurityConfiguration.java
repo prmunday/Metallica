a16f8189cfe3849775181665ed8ecf365ed14a5c
pauwarne <pauwarne@WKWUS8123840.global.publicisgroupe.net>
2018-10-11 16:53:34 +0530

    SecurityConfiguration now allows loading static files without auth.

--- APIGateway/src/main/java/com/metallica/APIGateway/SecurityConfiguration.java
+++ APIGateway/src/main/java/com/metallica/APIGateway/SecurityConfiguration.java
@@ -1,9 +1,15 @@
 package com.metallica.APIGateway;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.google.common.collect.ImmutableList;
 
 @Configuration
 @EnableOAuth2Sso
@@ -16,13 +22,31 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.disable()
 			.antMatcher("/**")
 			.authorizeRequests()
-			.antMatchers("/","/index.html")
+			.antMatchers("/static/**")
 				.permitAll()
 			.anyRequest()
 				.authenticated();
 		http
         .logout()
             .logoutUrl("/logout");
+		http.cors();
 	}
+	
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
